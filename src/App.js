a95edaadb4c9c3bbab6a5a3bbb185a34f9704b76
Zhanarbek Osmonaliev <zhanarbek.osmonaliev@stonybrook.edu>
2022-01-23 02:04:25 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+import Home from "./pages/home";
+import Site from "./pages/site";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/site" element={<Site />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/site.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Site = () => {
+  return (
+    <div>
+      <h1>Site</h1>
+    </div>
+  );
+};
+export default Site;
